bef84b5bc419a1046dcaf0fee2776931683472ef
FacundoC <facundocabrera2898@gmail.com>
2025-01-26 14:33:09 -0300

    pepito

--- Assets/BubbleBlowUpController.cs
+++ Assets/BubbleBlowUpController.cs
@@ -17,6 +17,7 @@ public class BubbleBlowUpController : MonoBehaviour
     public Animator sopaHeadanim;
     public float warningThreshold = .3f;
     private GameController gameController;
+    public SoundController soundController;
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -74,5 +75,7 @@ public class BubbleBlowUpController : MonoBehaviour
         anim.SetTrigger("pop");
         fallAfterPop.wasPopped = true;
         GetComponentInParent<BubbleController>().TriggerDestroy();
+        soundController.PlaySound("Pop");
+
     }
 }
